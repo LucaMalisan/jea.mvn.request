2eb8fa1f3234e80174f6ba871dcc4f33034d4be1
luca.malisan <luca.malisan@informaticon.com>
2024-06-04 09:20:56 +0200

    refactor: put json-split utils method in mvn.request instead of mvn.auth feat: enforce article permission in article module

--- src/main/java/json/JsonUtil.java
+++ src/main/java/json/JsonUtil.java
@@ -3,6 +3,9 @@ package json;
 import jakarta.json.*;
 
 import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class JsonUtil {
 
@@ -16,5 +19,12 @@ public class JsonUtil {
 
     public static String jsonValueToString(JsonValue value, String property) {
         return value.asJsonObject().get(property).toString().replace("\"", "");
+    }
+
+    public static Map<String, String> jsonStringToMap(String jsonStr) {
+        return Arrays.stream(jsonStr.split(","))
+                .map(e -> e.replaceAll("[{}\"]", ""))
+                .map(e -> e.split(":"))
+                .collect(Collectors.toMap(e -> e[0].trim(), e -> e[1].trim()));
     }
 }
